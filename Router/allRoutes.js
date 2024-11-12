766732f54398fb3d5850614ea9c1c05009e021a1
tatonkimono <a.hazmoune@9online.fr>
2024-11-12 10:11:46 +0100

    Gestion de l'afficahge des reservations

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,7 +3,13 @@ import Route from "./Route.js";
 //Définir ici vos routes
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
-    new Route("/galerie", "La galerie", "/pages/galerie.html"),];
+    new Route("/galerie", "La galerie", "/pages/galerie.html"),
+    new Route("/signin", "Connexion", "/pages/auth/signin.html"),
+    new Route("/signup", "Inscription", "/pages/auth/signup.html"),
+    new Route("/account", "Mon compte", "/pages/auth/account.html"),
+    new Route("/editpassword", "Changement de mot de passe", "/pages/auth/editpassword.html"),
+    new Route("/lesreservations", "Les reservations", "/pages/reservations/allresa.html"),
+];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Quai Antique";
